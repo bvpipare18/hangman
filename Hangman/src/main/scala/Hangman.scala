26af636878a9beeeb8cc20876ea359a51df6c383
Bharat Pipare <bharatpipare987@gmail.com>
2025-01-20 22:26:12 +0530

    02: add different mistake games status

--- Hangman/src/main/scala/Hangman.scala
+++ Hangman/src/main/scala/Hangman.scala
@@ -16,3 +16,85 @@ def setupGame() =
   val wordIndex = Random.nextInt(words.size)
   word = words(wordIndex).toUpperCase()
   println(word)
+  
+  printGameStatus()
+  
+def printGameStatus() =
+  mistakes match
+    case 0 => print0Mistakes()
+    case 1 => print1Mistake()
+    case 2 => print2Mistakes()
+    case 3 => print3Mistakes()
+    case 4 => print4Mistakes()
+    case 5 => print5Mistakes()
+    case 6 => print6Mistakes()
+
+def print0Mistakes() =
+  println("  |------|-")
+  println("  |      |")
+  println("  |       ")
+  println("  |")
+  println("  |")
+  println("  |")
+  println(" /|\\")
+  println("/ | \\")
+
+def print1Mistake() =
+  println("  |------|-")
+  println("  |      |")
+  println("  |      O")
+  println("  |")
+  println("  |")
+  println("  |")
+  println(" /|\\")
+  println("/ | \\")
+
+def print2Mistakes() =
+  println("  |------|-")
+  println("  |      |")
+  println("  |      O")
+  println("  |      |")
+  println("  |      |")
+  println("  |")
+  println(" /|\\")
+  println("/ | \\")
+
+def print3Mistakes() =
+  println("  |------|-")
+  println("  |      |")
+  println("  |      O")
+  println("  |     /|")
+  println("  |      |")
+  println("  |")
+  println(" /|\\")
+  println("/ | \\")
+
+def print4Mistakes() =
+  println("  |------|-")
+  println("  |      |")
+  println("  |      O")
+  println("  |     /|\\")
+  println("  |      |")
+  println("  |")
+  println(" /|\\")
+  println("/ | \\")
+
+def print5Mistakes() =
+  println("  |------|-")
+  println("  |      |")
+  println("  |      O")
+  println("  |     /|\\")
+  println("  |      |")
+  println("  |     /")
+  println(" /|\\")
+  println("/ | \\")
+
+def print6Mistakes() =
+  println("  |------|-")
+  println("  |      |")
+  println("  |      O")
+  println("  |     /|\\")
+  println("  |      |")
+  println("  |     / \\")
+  println(" /|\\")
+  println("/ | \\")
