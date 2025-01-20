65b5033dbe816f6e1d96b61c37fa4567d30bd0c5
Bharat Pipare <bharatpipare987@gmail.com>
2025-01-20 23:17:52 +0530

    04: implement game algorithm

--- Hangman/src/main/scala/Hangman.scala
+++ Hangman/src/main/scala/Hangman.scala
@@ -1,4 +1,5 @@
 import scala.collection.mutable.ArrayBuffer
+import scala.io.StdIn
 import scala.util.Random
 
 val words = List("elbow", "writer", "circle", "polish", "bridge", "store", "fang", "scarecrow", "show", "jeans", "wilderness", "attempt", "waxing", "aftermath", "banana", "wrist", "wheel", "spring", "cherries", "nerve")
@@ -16,12 +17,34 @@ def setupGame() =
   val wordIndex = Random.nextInt(words.size)
   word = words(wordIndex).toUpperCase()
   println(word)
-  
+
   for (i <- word.indices)
     guesses.addOne('_')
-  
-  printGameStatus()
-  
+
+  var gameOver = false
+
+  while (!gameOver)
+    printGameStatus()
+    println("Please enter a letter:")
+    val input = StdIn.readLine()
+
+    if(input.isEmpty)
+      println("Invalid input, Please try again!!")
+    else
+      val letter = input.charAt(0).toUpper
+      if(word.contains(letter))
+        for (i <- word.indices)
+          if(word.charAt(i) == letter)
+            guesses(i) = letter
+        if (!guesses.contains('_'))
+          gameOver = true
+      else
+        println("Sorry, that's not part of the word")
+        remainingGuesses -= 1
+        mistakes += 1
+        if(mistakes == 6)
+          gameOver = true
+
 def printGameStatus() =
   mistakes match
     case 0 => print0Mistakes()
@@ -31,11 +54,11 @@ def printGameStatus() =
     case 4 => print4Mistakes()
     case 5 => print5Mistakes()
     case 6 => print6Mistakes()
-    
+
     print("Word: ")
     for (element <- guesses)
       print(s"$element ")
-      
+
     println(s"\nYou have $remainingGuesses guess(es) left")
 
 def print0Mistakes() =
